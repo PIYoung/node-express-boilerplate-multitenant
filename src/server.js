0cabc7d9050ffdb8d185e629b7c08e6f649c10b5
InYoung Park <dlsdudg15@naver.com>
2023-01-18 23:40:47 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import app from './configs/express.config.js';
 
 function bootstrap() {
-  const port = process.env.PORT;
+  const { PORT: port } = process.env;
   const sever = app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
