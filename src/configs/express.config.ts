0766f66eeb648dba34142de7c15f1ff0a5a70206
InYoung Park <dlsdudg15@naver.com>
2023-01-24 20:37:49 +0900

    Update express configuration

--- src/configs/express.config.ts
+++ src/configs/express.config.ts
@@ -1,11 +1,24 @@
 import express from 'express';
+import compression from 'compression';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import passport from 'passport';
 
 import apiRouter from '../routes/index.js';
 
 const app = express();
 
+app.use(morgan('dev'));
+
+app.use(compression());
+app.use(cors());
+app.use(helmet());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(passport.initialize());
 
 app.use('/', apiRouter);
 
