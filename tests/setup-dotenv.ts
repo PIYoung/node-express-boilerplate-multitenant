9e52f5bc9cb96722b5e0bb4d94605dcb7c17246b
InYoung Park <dlsdudg15@naver.com>
2023-02-22 13:16:38 +0900

    test: remove useless setup files

--- tests/setup-dotenv.ts
+++
@@ -1,8 +0,0 @@
-import appRootPath from 'app-root-path';
-import dotenv from 'dotenv';
-import { join } from 'path';
-
-const { path } = appRootPath;
-
-dotenv.config();
-dotenv.config({ path: join(path, '.env.test') });
