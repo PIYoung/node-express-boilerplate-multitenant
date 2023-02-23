55b30a8c8198fb480c30d81ba6aedc3b9348eaa8
InYoung Park <dlsdudg15@naver.com>
2023-02-23 14:39:56 +0900

    feat: log memory usage

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,21 @@
 import app from './configs/express.config';
-
 import logger from './configs/logger.config';
 import { connectPostgres } from './configs/postgres.config';
 import { configDotenv } from './configs/dotenv.config';
+
+const readMemory = () => {
+  const memory = process.memoryUsage();
+  const convert = {
+    Kb: (n: number) => n / 1024,
+    Mb: (n: number) => convert.Kb(n) / 1024,
+    Gb: (n: number) => convert.Mb(n) / 1024,
+  };
+  const toHuman = (n: number, t: keyof typeof convert) => `${convert[t](n).toFixed(2)}${t}`;
+  return `Used ${toHuman(memory.heapUsed, 'Mb')} of ${toHuman(memory.heapTotal, 'Mb')} - RSS: ${toHuman(
+    memory.rss,
+    'Mb',
+  )}`;
+};
 
 const bootstrap = async () => {
   try {
@@ -11,7 +24,8 @@ const bootstrap = async () => {
     await connectPostgres();
 
     app.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
+      logger.info(`🚀 Server is running on port ${port}`);
+      logger.info(`🚀 Starting server... ${readMemory()}`);
     });
   } catch (err) {
     logger.error(err);
