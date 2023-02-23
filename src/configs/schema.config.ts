378ecba0954298672867c8159670ba31cd13a84b
InYoung Park <dlsdudg15@naver.com>
2023-02-23 17:22:38 +0900

    feat: add some functions related schema

--- src/configs/schema.config.ts
+++ src/configs/schema.config.ts
@@ -1,6 +1,9 @@
-type Schema = {
+import { Sequelize } from 'sequelize-typescript';
+
+export type TenantSchema = `tenant_${number}`;
+export type Schema = {
   main: 'main';
-  [key: `TENANT_SCHEMA_PREFIX${number}`]: `tenant_${number}`;
+  [key: TenantSchema]: TenantSchema;
 };
 
 export const DEFAULT_MAIN_SCHEMA = 'main';
@@ -8,3 +11,12 @@ export const TENANT_SCHEMA_PREFIX = 'tenant_';
 export const ALL_SCHEMAS: Schema = {
   [DEFAULT_MAIN_SCHEMA]: DEFAULT_MAIN_SCHEMA,
 };
+
+export const createSchema = async (seq: Sequelize, schema: keyof Schema) => {
+  await seq.createSchema(schema, { logging: false });
+  await seq.query(`GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA ${schema} TO ${process.env['POSTGRES_USER']};`, {
+    logging: false,
+  });
+};
+
+export const getTenantSchema = (tenantId: number) => `${TENANT_SCHEMA_PREFIX}${tenantId}` as TenantSchema;
