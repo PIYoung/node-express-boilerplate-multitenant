c1b53582352f36b6a6d2770db2813e2c33c94057
InYoung Park <dlsdudg15@naver.com>
2023-02-23 17:42:02 +0900

    feat: sync tenant models

--- src/models/tenant/index.ts
+++ src/models/tenant/index.ts
@@ -1,9 +1,23 @@
-import { Sequelize } from 'sequelize-typescript';
+import { ModelCtor, Sequelize } from 'sequelize-typescript';
 
 import { ALL_SCHEMAS, createSchema, TenantSchema } from '../../configs/schema.config';
+import { GroupRole } from './group-role.model';
+import { Group } from './group.model';
+import { UserInfo } from './user-info.model';
+import { UserRole } from './user-role.model';
 
 export async function generateTenantModels(seq: Sequelize, schema: TenantSchema) {
   await createSchema(seq, schema);
 
   ALL_SCHEMAS[schema] = schema;
+
+  const models: ModelCtor[] = [GroupRole, Group, UserInfo, UserRole];
+
+  seq.addModels(models);
+
+  models.forEach((model) => {
+    model.options.schema = schema;
+  });
+
+  await seq.sync({ alter: true });
 }
